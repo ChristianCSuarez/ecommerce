05a18b80af7a807a386cc5055de40eb278d50105
DanteJose <chris8610@gmail.com>
2025-01-02 10:45:54 -0500

    Maquetando Product Detail

--- ecommerce/src/Context/index.jsx
+++ ecommerce/src/Context/index.jsx
@@ -4,12 +4,26 @@ export const ShoppingCardContext = createContext();
 
 export const ShoppingCardProvider = ({children}) => {
 
+    //Shopping Cart - Increment Quantity
     const [count, setCount] = useState(0)
+
+    //Product Detail - Open/Close
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+    const openProductDetail = () => setIsProductDetailOpen(true)
+    const closeProductDetail = () => setIsProductDetailOpen(false)
+
+    //Product Detail - Show Content
+    const [productToShow, setProductToShow ] = useState({})
 
     return (
         <ShoppingCardContext.Provider value={{
             count,
-            setCount
+            setCount,
+            openProductDetail,
+            closeProductDetail,
+            isProductDetailOpen,
+            productToShow,
+            setProductToShow
         }}>
             {children}
         </ShoppingCardContext.Provider>
