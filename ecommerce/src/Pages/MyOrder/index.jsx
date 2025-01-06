da446d347f274c913d252ba17224c292ce79c302
DanteJose <chris8610@gmail.com>
2025-01-06 11:47:23 -0500

    Lista de órdenes

--- ecommerce/src/Pages/MyOrder/index.jsx
+++ ecommerce/src/Pages/MyOrder/index.jsx
@@ -1,7 +1,9 @@
 import { useContext } from 'react'
+import { Link } from "react-router-dom"
 import {ShoppingCardContext} from '../../Context'
 import OrderCard from '../../Components/OrderCard'
 import Layout from "../../Components/Layout"
+import {  ChevronLeftIcon } from '@heroicons/react/24/solid'
 
 function MyOrder() {
 
@@ -9,7 +11,12 @@ function MyOrder() {
   
   return (
       <Layout>
-        MyOrder
+         <div className="flex items-center justify-center relative w-80 mb-6">
+          <Link to={'/my-orders'} className="absolute left-0 ¿">
+            <ChevronLeftIcon className="size-6 text-black cursor-pointer" />
+          </Link>
+          <h1>My Order</h1>
+        </div>
         <div className='flex flex-col w-80'>
             {context.order?.slice(-1)[0].products.map(product => (
                     <OrderCard 
